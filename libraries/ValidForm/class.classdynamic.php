b98d1e35a31c7810479ed604a74af9b3a57db554
flangfeldt@felix-it.com <flangfeldt@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2009-01-09 04:45:23 +0000

    Changed to the MIT license
    git-svn-id: https://validformbuilder.googlecode.com/svn/trunk@29 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.classdynamic.php
+++ libraries/ValidForm/class.classdynamic.php
@@ -1,4 +1,11 @@
 <?php
+/***************************
+ * This file is part of ValidForm Builder - build valid and secure web forms quickly
+ * <http://code.google.com/p/validformbuilder/>
+ * Copyright (c) 2009 Felix Langfeldt
+ * 
+ * This software is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+ ***************************/
 
 class ClassDynamic {
 
--- libraries/ValidForm/class.vf_select.php
+++ libraries/ValidForm/class.vf_select.php
@@ -1,22 +1,11 @@
 <?php
-/**
- * This file is part of ValidFormBuilder.
- *
- * Copyright (c) 2008 Felix Langfeldt
- *
- * ValidFormBuilder is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+/***************************
+ * This file is part of ValidForm Builder - build valid and secure web forms quickly
+ * <http://code.google.com/p/validformbuilder/>
+ * Copyright (c) 2009 Felix Langfeldt
  * 
- * ValidFormBuilder is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with ValidFormBuilder.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * This software is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+ ***************************/
  
 /**
  * VF_Select class
