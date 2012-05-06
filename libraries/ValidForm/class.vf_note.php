40176855bb9b01eecbe832611876c1326524f190
flangfeldt@felix-it.com <flangfeldt@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2012-05-06 19:09:51 +0000

    Fixed incorrect license information. Removed the jQuery library from the repository. It should be included using one of their CDN links.
    git-svn-id: https://validformbuilder.googlecode.com/svn/trunk@215 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.vf_note.php
+++ libraries/ValidForm/class.vf_note.php
@@ -5,12 +5,12 @@
  * Copyright (c) 2009-2012, Felix Langfeldt <flangfeldt@felix-it.com>.
  * All rights reserved.
  * 
- * This software is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * This software is released under the GNU GPL v2 License <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * 
  * @package    ValidForm
  * @author     Felix Langfeldt <flangfeldt@felix-it.com>
  * @copyright  2009-2012 Felix Langfeldt <flangfeldt@felix-it.com>
- * @license    http://www.opensource.org/licenses/mit-license.php
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU GPL v2
  * @link       http://code.google.com/p/validformbuilder/
  ***************************/
   
--- libraries/validform.js
+++ libraries/validform.js
@@ -4,22 +4,15 @@
  * Copyright (c) 2009-2012, Felix Langfeldt <flangfeldt@felix-it.com>.
  * All rights reserved.
  * 
- * This software is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * This software is released under the GNU GPL v2 License <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * 
  * @package    ValidForm
  * @author     Felix Langfeldt <flangfeldt@felix-it.com>
  * @copyright  2009-2012 Felix Langfeldt <flangfeldt@felix-it.com>
- * @license    http://www.opensource.org/licenses/mit-license.php
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU GPL v2
  * @link       http://code.google.com/p/validformbuilder/
+ * @version    Release: 0.2.4
  ***************************/
-
-/**
- * ValidForm class
- *
- * @package ValidForm
- * @author Felix Langfeldt
- * @version 0.2.4
- */
 
 function ValidFormValidator(strFormId) {
 	/****************************/
