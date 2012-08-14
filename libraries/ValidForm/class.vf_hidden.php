cc53fc262eef9cd31d71c8725847e08f711248e6
rvanbaalen@felix-it.com <rvanbaalen@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2012-08-14 19:52:39 +0000

    Nothing to report here.
    git-svn-id: https://validformbuilder.googlecode.com/svn/trunk@252 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.vf_hidden.php
+++ libraries/ValidForm/class.vf_hidden.php
@@ -13,7 +13,7 @@
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU GPL v2
  * @link       http://code.google.com/p/validformbuilder/
  ***************************/
- 
+
 require_once('class.vf_element.php');
 
 /**
