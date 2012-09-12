0f27d11445de645e878815a7e9725ff2c458a55b
rvanbaalen@felix-it.com <rvanbaalen@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2012-09-12 21:48:38 +0000

    Triggerfield update
    git-svn-id: https://validformbuilder.googlecode.com/svn/branches/Client-side Wizard@318 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.vf_password.php
+++ libraries/ValidForm/class.vf_password.php
@@ -1,25 +1,25 @@
 <?php
 /***************************
  * ValidForm Builder - build valid and secure web forms quickly
- * 
+ *
  * Copyright (c) 2009-2012, Felix Langfeldt <flangfeldt@felix-it.com>.
  * All rights reserved.
- * 
+ *
  * This software is released under the GNU GPL v2 License <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
- * 
+ *
  * @package    ValidForm
  * @author     Felix Langfeldt <flangfeldt@felix-it.com>
  * @copyright  2009-2012 Felix Langfeldt <flangfeldt@felix-it.com>
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU GPL v2
  * @link       http://code.google.com/p/validformbuilder/
  ***************************/
-  
+
 require_once('class.vf_element.php');
 
 /**
- * 
+ *
  * Password Class
- * 
+ *
  * @package ValidForm
  * @author Felix Langfeldt
  * @version Release: 0.2.2
@@ -29,16 +29,16 @@ class VF_Password extends VF_Element {
 
 	public function toHtml($submitted = FALSE, $blnSimpleLayout = FALSE, $blnLabel = true, $blnDisplayError = true) {
 		$strOutput = "";
-		
+
 		if (!$blnSimpleLayout) {
 			$blnError = ($submitted && !$this->__validator->validate() && $blnDisplayError) ? TRUE : FALSE;
-			
+
 			$strClass = ($this->__validator->getRequired()) ? "vf__required" : "vf__optional";
 			$strClass = ($blnError) ? $strClass . " vf__error" : $strClass;
 			$strOutput = "<div class=\"{$strClass}\">\n";
-			
+
 			if ($blnError) $strOutput .= "<p class=\"vf__error\">{$this->__validator->getError()}</p>";
-			
+
 			if ($blnLabel) {
 				$strLabel = (!empty($this->__requiredstyle) && $this->__validator->getRequired()) ? sprintf($this->__requiredstyle, $this->__label) : $this->__label;
 				if (!empty($this->__label)) $strOutput .= "<label for=\"{$this->__id}\"{$this->__getLabelMetaString()}>{$strLabel}</label>\n";
@@ -51,37 +51,37 @@ class VF_Password extends VF_Element {
 		if ($this->__validator->getMaxLength() > 0) {
 			$this->__meta["maxlength"] = $this->__validator->getMaxLength();
 		}
-		
+
 		$strOutput .= "<input type=\"password\" value=\"{$this->__getValue($submitted)}\" name=\"{$this->__name}\" id=\"{$this->__id}\" {$this->__getMetaString()} />\n";
-		
+
 		if (!empty($this->__tip)) $strOutput .= "<small class=\"vf__tip\">{$this->__tip}</small>\n";
 		$strOutput .= "</div>\n";
-		
+
 		return $strOutput;
 	}
-	
-	public function toJS($strId = null) {
+
+	public function toJS() {
 		$strCheck = $this->__validator->getCheck();
 		$strCheck = (empty($strCheck)) ? "''" : str_replace("'", "\\'", $strCheck);
 		$strRequired = ($this->__validator->getRequired()) ? "true" : "false";;
 		$intMaxLength = ($this->__validator->getMaxLength() > 0) ? $this->__validator->getMaxLength() : "null";
 		$intMinLength = ($this->__validator->getMinLength() > 0) ? $this->__validator->getMinLength() : "null";
 
-		
+
 		$strOutput = "objForm.addElement('{$this->__id}', '{$this->__name}', {$strCheck}, {$strRequired}, {$intMaxLength}, {$intMinLength}, '" . addslashes($this->__validator->getFieldHint()) . "', '" . addslashes($this->__validator->getTypeError()) . "', '" . addslashes($this->__validator->getRequiredError()) . "', '" . addslashes($this->__validator->getHintError()) . "', '" . addslashes($this->__validator->getMinLengthError()) . "', '" . addslashes($this->__validator->getMaxLengthError()) . "');\n";
-		
+
 		$objMatchWith = $this->getValidator()->getMatchWith();
 		if (is_object($objMatchWith)) {
 			$strOutput .= "objForm.matchfields('" . $this->__id . "', '" . $objMatchWith->getId() . "', '" . $this->__validator->getMatchWithError() . "');\n";
 		}
 
 		if ($this->hasTrigger()) {
-			$strOutput .= $this->addTriggerJs($strId);
+			$strOutput .= $this->addTriggerJs();
 		}
 
 		return $strOutput;
 	}
-	
+
 }
 
 ?>
