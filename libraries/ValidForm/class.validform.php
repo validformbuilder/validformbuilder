ace1aa80ae34327279a8fe516a905bae4b443f77
rvanbaalen@felix-it.com <rvanbaalen@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2013-01-21 20:28:07 +0000

    Updated copyright and author info Added reference to vf_constants
    git-svn-id: https://validformbuilder.googlecode.com/svn/trunk@382 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.validform.php
+++ libraries/ValidForm/class.validform.php
@@ -2,16 +2,20 @@
 /***************************
  * ValidForm Builder - build valid and secure web forms quickly
  *
- * Copyright (c) 2009-2012, Felix Langfeldt <flangfeldt@felix-it.com>.
+ * Copyright (c) 2009-2013
+ *
+ * Felix Langfeldt <flangfeldt@felix-it.com>
+ * Robin van Baalen <rvanbaalen@felix-it.com>
+ *
  * All rights reserved.
  *
  * This software is released under the GNU GPL v2 License <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  *
  * @package    ValidForm
- * @author     Felix Langfeldt <flangfeldt@felix-it.com>
- * @copyright  2009-2012 Felix Langfeldt <flangfeldt@felix-it.com>
+ * @author     Felix Langfeldt <flangfeldt@felix-it.com>, Robin van Baalen <rvanbaalen@felix-it.com>
+ * @copyright  2009-2013 Felix Langfeldt <flangfeldt@felix-it.com>, Robin van Baalen <rvanbaalen@felix-it.com>
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU GPL v2
- * @link       http://code.google.com/p/validformbuilder/
+ * @link       http://validformbuilder.org
  ***************************/
 
 require_once('class.classdynamic.php');
@@ -36,27 +40,7 @@ require_once('class.vf_captcha.php');
 require_once('class.vf_fieldvalidator.php');
 require_once('class.vf_page.php');
 
-define('VFORM_STRING', 1);
-define('VFORM_TEXT', 2);
-define('VFORM_NUMERIC', 3);
-define('VFORM_INTEGER', 4);
-define('VFORM_WORD', 5);
-define('VFORM_EMAIL', 6);
-define('VFORM_PASSWORD', 7);
-define('VFORM_SIMPLEURL', 8);
-define('VFORM_FILE', 9);
-define('VFORM_BOOLEAN', 10);
-define('VFORM_CAPTCHA', 11);
-define('VFORM_RADIO_LIST', 12);
-define('VFORM_CHECK_LIST', 13);
-define('VFORM_SELECT_LIST', 14);
-define('VFORM_PARAGRAPH', 15);
-define('VFORM_CURRENCY', 16);
-define('VFORM_DATE', 17);
-define('VFORM_CUSTOM', 18);
-define('VFORM_CUSTOM_TEXT', 19);
-define('VFORM_HTML', 20);
-define('VFORM_URL', 21);
+require_once('vf_constantes.php');
 
 /**
  *
