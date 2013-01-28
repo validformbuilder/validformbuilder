f56824d52fb805f6398a7e3108c2c3fa16e49227
rvanbaalen@felix-it.com <rvanbaalen@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2013-01-28 19:30:05 +0000

    Re-added unittests to branch
    git-svn-id: https://validformbuilder.googlecode.com/svn/branches/phpunittest@405 926da7a8-d6ea-11dd-b97a-b9dcd130504c

---
+++ tests/README.md
@@ -0,0 +1,15 @@
+Run the unit tests
+==================
+
+Run ValidForm Builder unit tests from the command line using:
+
+cd /path/to/validform/tests
+phpunit -c phpunit.xml
+
+
+Install PHP Unit
+================
+If you don't have PHPUnit up and running on your system yet, 
+check out the official PHPUnit documentation on how to get started:
+
+http://www.phpunit.de/manual/current/en/installation.htm
---
+++ tests/bootstrap.php
@@ -0,0 +1,104 @@
+<?php
+
+set_include_path(get_include_path() .
+	PATH_SEPARATOR . "/Users/robin/Sites/validformbuilder/trunk/libraries/ValidForm");
+
+// misc functions
+function autoload($class_name) {
+	$strClass = 'class.' . strtolower($class_name) . '.php';
+	if (include_exists($strClass)) {
+		require_once($strClass);
+		return;
+	}
+
+	$strClass = implode('/', explode('_', $class_name)) . '.php';
+	if (include_exists($strClass)) {
+		require_once($strClass);
+		return;
+	}
+
+	$arrClass = explode("\\", $class_name);
+	$strClass = "class." . strtolower(array_pop($arrClass)) . ".php";
+	if (include_exists($strClass)) {
+		require_once($strClass);
+		return;
+	}
+}
+
+spl_autoload_register("autoload");
+
+function include_exists($file) {
+   static $include_dirs = null;
+   static $include_path = null;
+
+   // set include_dirs
+   if (is_null($include_dirs) || get_include_path() !== $include_path) {
+	   $include_path    = get_include_path();
+	   foreach (explode(PATH_SEPARATOR, $include_path) as $include_dir) {
+		   if (substr($include_dir, -1) != '/') {
+			   $include_dir .= '/';
+		   }
+		   $include_dirs[]    = $include_dir;
+	   }
+   }
+
+   if (substr($file, 0, 1) == '/') { //absolute filepath - what about file:///?
+	   return (file_exists($file));
+   }
+
+//   if ((substr($file, 0, 7) == 'http://' || substr($file, 0, 6) == 'ftp://') && ini_get('allow_url_fopen')) {
+//	   return true;
+//   }
+
+   foreach ($include_dirs as $include_dir) {
+	   if (file_exists($include_dir.$file)) {
+		   return true;
+	   }
+   }
+
+   return false;
+}
+
+/**
+ * Class to hold all randomize logic.
+ */
+class Random {
+	/**
+	 * Create a random string
+	 * @param  integer $length String length
+	 * @return string          The random string
+	 */
+	public static function string ($length = 10) {
+		$randstr = "";
+
+		for ($i = 0; $i < $length; $i++) {
+			$randnum = mt_rand(0,61);
+
+			if($randnum < 10) {
+				$randstr .= chr($randnum+48);
+
+			} else if($randnum < 36) {
+				$randstr .= chr($randnum+55);
+
+			} else {
+				$randstr .= chr($randnum+61);
+
+			}
+		}
+
+		return $randstr;
+	}
+}
+
+if (!defined("VFORM_TEXT")) {
+	require_once("vf_constants.php");
+}
+
+
+echo "============================================================\n";
+echo "Include path set to:\n";
+foreach (explode(PATH_SEPARATOR, get_include_path()) as $strPath) {
+	echo $strPath . "\n";
+}
+echo "============================================================\n\n";
+?>
