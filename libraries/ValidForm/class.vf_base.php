ea5650736e687fe7a54d098c8668c6d46323821d
rvanbaalen@felix-it.com <rvanbaalen@felix-it.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2013-02-05 19:17:22 +0000

    Added conditional logic to base of all main classes
    git-svn-id: https://validformbuilder.googlecode.com/svn/branches/conditionals@435 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.vf_base.php
+++ libraries/ValidForm/class.vf_base.php
@@ -4,6 +4,84 @@
  * All ValidForm classes share this base logic.
  */
 class VF_Base extends ClassDynamic {
+	protected $__conditions = array();
 
+	/**
+	 * Add a new condition to the current field
+	 * @param [type] $strType           [description]
+	 * @param [type] $blnValue          [description]
+	 * @param [type] $arrComparisons    [description]
+	 * @param [type] $intComparisonType [description]
+	 */
+	public function addCondition($strType, $blnValue, $arrComparisons, $intComparisonType = VFORM_MATCH_ANY) {
+		if ($this->hasCondition($strType)) {
+			// Get an existing condition if it's already there.
+			$objCondition = $this->getCondition($strType);
+		} else {
+			// Add a new one if this condition type doesn't exist yet.
+			$objCondition = new VF_Condition($this, $strType, $blnValue, $intComparisonType);
+		}
+
+		if (is_array($arrComparisons) && count($arrComparisons) > 0) {
+			foreach ($arrComparisons as $arrComparison) {
+				if (is_array($arrComparison)) {
+					try {
+						$objCondition->addComparison($arrComparison);
+					} catch (InvalidArgumentException $e) {
+						throw new Exception("Could not set condition: " . $e->getMessage(), 1);
+					}
+				} else {
+					throw new InvalidArgumentException("Invalid or no comparison(s) supplied.", 1);
+				}
+			}
+
+			array_push($this->__conditions, $objCondition);
+		} else {
+			throw new InvalidArgumentException("Invalid or no comparison(s) supplied.", 1);
+		}
+	}
+
+	/**
+	 * Get element's VF_Condition object
+	 * Note: When chaining methods, always use hasCondition() first before chaining
+	 * for example 'getCondition()->getResult()'.
+	 * @param  String $strType 		Condition type e.g. 'required', 'visibile' and 'disabled'
+	 * @return VF_Condition|null    The found condition or null if no condition is found.
+	 */
+	public function getCondition($strType) {
+		$objConditions = $this->getConditions();
+		$objCondition = null;
+
+		foreach ($objConditions as $objCondition) {
+			if ($objCondition->getType() === strtolower($strType)) {
+				break;
+			}
+		}
+
+		return $objCondition;
+	}
+
+
+	/**
+	 * Check if the current fields contains a condition object
+	 * @param  String  $strType Condition type (e.g. 'required', 'disabled', 'visible' etc.)
+	 * @return boolean          True if element has condition object set, false if not
+	 */
+	public function hasCondition($strType) {
+		$blnReturn = false;
+
+		foreach ($this->__conditions as $objCondition) {
+			if ($objCondition->getType() === strtolower($strType)) {
+				$blnReturn = true;
+				break;
+			}
+		}
+
+		return $blnReturn;
+	}
+
+	public function hasConditions() {
+		return (count($this->__conditions) > 0);
+	}
 }
 ?>
