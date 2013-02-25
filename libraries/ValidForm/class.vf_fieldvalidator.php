c152cb624013fa79610aaa869c4e381bae24640e
robin@neverwoods.com <robin@neverwoods.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2013-02-25 12:44:59 +0000

    Fixed major bug in fieldvalidator
    git-svn-id: https://validformbuilder.googlecode.com/svn/branches/conditionals@469 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.vf_fieldvalidator.php
+++ libraries/ValidForm/class.vf_fieldvalidator.php
@@ -169,7 +169,7 @@ class VF_FieldValidator extends ClassDynamic {
 
 		// Check if parent element is an area.
 		// If so, check if it's an active area that is selected
-		$objParent = $this->getMeta("parent", null);
+		$objParent = $this->__field->getMeta("parent", null);
 		if (!is_null($objParent) && get_class($objParent) === "VF_Area") {
 			if ($objParent->__active && $objParent->hasContent($intCount)) $this->setFieldMeta("checked", "checked", true);
 		}
