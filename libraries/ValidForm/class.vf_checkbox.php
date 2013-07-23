9744e5a80623de499e7e26c9a713aef3369d6de4
felix@neverwoods.com <felix@neverwoods.com@926da7a8-d6ea-11dd-b97a-b9dcd130504c>
2013-07-23 03:19:01 +0000

    Enabled MultiField support on a checkbox. Now it's possible to build MultiFields that can be "enabled" and than filled using Conditions.
    git-svn-id: https://validformbuilder.googlecode.com/svn/trunk@593 926da7a8-d6ea-11dd-b97a-b9dcd130504c

--- libraries/ValidForm/class.vf_checkbox.php
+++ libraries/ValidForm/class.vf_checkbox.php
@@ -34,34 +34,58 @@ class VF_Checkbox extends VF_Element {
 	public function toHtml($submitted = FALSE, $blnSimpleLayout = FALSE, $blnLabel = true, $blnDisplayErrors = true) {
 		$blnError = ($submitted && !$this->__validator->validate() && $blnDisplayErrors) ? TRUE : FALSE;
 
-		//*** We asume that all dynamic fields greater than 0 are never required.
-		if ($this->__validator->getRequired()) {
-			$this->setMeta("class", "vf__required");
+		if (!$blnSimpleLayout) {
+			//*** We asume that all dynamic fields greater than 0 are never required.
+		    if ($this->__validator->getRequired()) {
+		        $this->setMeta("class", "vf__required");
+		    } else {
+		        $this->setMeta("class", "vf__optional");
+		    }
+		
+		    if ($blnError) $this->setMeta("class", "vf__error");
+		    if (!$blnLabel) $this->setMeta("class", "vf__nolabel");
+		
+		    // Call this right before __getMetaString();
+		    $this->setConditionalMeta();
+		
+		    $strOutput = "<div{$this->__getMetaString()}>\n";
+		
+		    if ($blnError) $strOutput .= "<p class=\"vf__error\">{$this->__validator->getError()}</p>";
+		
+		    if ($this->__getValue($submitted)) {
+		        //*** Add the "checked" attribute to the input field.
+		        $this->setFieldMeta("checked", "checked");
+		    } else {
+		        //*** Remove the "checked" attribute from the input field. Just to be sure it wasn't set before.
+		        $this->setFieldMeta("checked", null, TRUE);
+		    }
+		
+		    if ($blnLabel) {
+		        $strLabel = (!empty($this->__requiredstyle) && $this->__validator->getRequired()) ? sprintf($this->__requiredstyle, $this->__label) : $this->__label;
+		        if (!empty($this->__label)) $strOutput .= "<label for=\"{$this->__id}\"{$this->__getLabelMetaString()}>{$strLabel}</label>\n";
+		    }
 		} else {
-			$this->setMeta("class", "vf__optional");
+		    if ($blnError) $this->setMeta("class", "vf__error");
+		    $this->setMeta("class", "vf__multifielditem");
+
+		    // Call this right before __getMetaString();
+		    $this->setConditionalMeta();
+
+		    $strOutput = "<div{$this->__getMetaString()}>\n";
+
+		    if ($this->__getValue($submitted)) {
+		        //*** Add the "checked" attribute to the input field.
+		        $this->setFieldMeta("checked", "checked");
+		    } else {
+		        //*** Remove the "checked" attribute from the input field. Just to be sure it wasn't set before.
+		        $this->setFieldMeta("checked", null, TRUE);
+		    }
 		}
 
-		if ($blnError) $this->setMeta("class", "vf__error");
-
-		// Call this right before __getMetaString();
-		$this->setConditionalMeta();
-
-		$strOutput = "<div{$this->__getMetaString()}>\n";
-
-		if ($blnError) $strOutput .= "<p class=\"vf__error\">{$this->__validator->getError()}</p>";
-
-		if ($this->__getValue($submitted)) {
-			//*** Add the "checked" attribute to the input field.
-			$this->setFieldMeta("checked", "checked");
-		} else {
-			//*** Remove the "checked" attribute from the input field. Just to be sure it wasn't set before.
-			$this->setFieldMeta("checked", null, TRUE);
-		}
-
-		$strLabel = (!empty($this->__requiredstyle) && $this->__validator->getRequired()) ? sprintf($this->__requiredstyle, $this->__label) : $this->__label;
-		if (!empty($this->__label)) $strOutput .= "<label for=\"{$this->__id}\"{$this->__getLabelMetaString()}>{$strLabel}</label>\n";
 		$strOutput .= "<input type=\"checkbox\" name=\"{$this->__name}\" id=\"{$this->__id}\"{$this->__getFieldMetaString()}/>\n";
+
 		if (!empty($this->__tip)) $strOutput .= "<small class=\"vf__tip\">{$this->__tip}</small>\n";
+
 		$strOutput .= "</div>\n";
 
 		return $strOutput;
