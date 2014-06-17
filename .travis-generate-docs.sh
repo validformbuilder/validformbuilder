c861370d9c5a4d8989f1147f37e8a947aa2b4f34
Robin van Baalen <robin@neverwoods.com>
2014-06-17 02:09:10 -0400

    Another test

--- .travis-generate-docs.sh
+++ .travis-generate-docs.sh
@@ -4,25 +4,24 @@ echo "Generating docs with PHPDocumentor..."
 php phpDocumentor.phar
 
 echo "Going home $HOME"
-cd "$HOME"
+cd ~
 #git config --global user.email "travis@travis-ci.org"
 #git config --global user.name "travis-ci"
 git clone --branch=gh-pages https://github.com/neverwoods/validformbuilder.git gh-pages || exit 1
 
 echo "Entering gh-pages"
-cd $HOME/gh-pages
-
-mkdir docs
+cd gh-pages
 
 git rm -rf .
 
 echo "Copy generated docs from $TRAVIS_BUILD_DIR/docs/* to $HOME/gh-pages/docs"
 #cp -Rf $TRAVIS_BUILD_DIR/docs/* $HOME/gh-pages/docs
 
-ls -la $HOME/gh-pages
+echo "Current dir"
+ls -la .
 
 echo "One level deeper"
-ls -la $HOME/gh-pages/docs
+ls -la ./docs
 
 #git add .
 #git commit -F- <<EOF
