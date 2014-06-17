9330babdbc1977a35d6fcd15c931cd27b115e64a
Robin van Baalen <robin@neverwoods.com>
2014-06-17 09:12:37 -0400

    Final version?
    Signed-off-by: Robin van Baalen <robin@neverwoods.com>

--- .travis-generate-docs.sh
+++ .travis-generate-docs.sh
@@ -5,15 +5,15 @@ php phpDocumentor.phar
 
 echo "Going home $HOME"
 cd ~
-#git config --global user.email "travis@travis-ci.org"
-#git config --global user.name "travis-ci"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
 git clone --branch=gh-pages https://github.com/neverwoods/validformbuilder.git gh-pages || exit 1
 
 echo "Entering gh-pages"
 cd gh-pages/docs
 
 git rm -r **/*
-touch placeholder
+touch placeholder #adding a placeholder keeps the docs folder
 git rm *.*
 
 cd ..
@@ -21,16 +21,14 @@ cd ..
 echo "Copy generated docs from $TRAVIS_BUILD_DIR/docs/* to ./docs"
 cp -Rf $TRAVIS_BUILD_DIR/docs/* ./docs
 
+rm docs/placeholder #docs is filled, no need for placeholder anymore
 
-ls -la ./docs
+git add -all
 
-#git add -all
+git commit -F- <<EOF
+Latest docs on successful travis build $TRAVIS_BUILD_NUMBER
+ValidForm Builder commit $TRAVIS_COMMIT
+EOF
 
-#git commit -F- <<EOF
-#Latest docs on successful travis build $TRAVIS_BUILD_NUMBER
-
-#ValidForm Builder commit $TRAVIS_COMMIT
-#EOF
-#git push origin gh-pages
-
+git push origin gh-pages
 echo "Published docs to gh-pages."
