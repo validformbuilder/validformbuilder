99adde1632ac708229a03f84744d61da98ddf2ec
Robin van Baalen <robin@neverwoods.com>
2014-06-17 00:56:09 -0400

    Update .travis-generate-docs.sh

--- .travis-generate-docs.sh
+++ .travis-generate-docs.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 echo "Generating docs with PHPDocumentor..."
-
 php phpDocumentor.phar
 
+echo "Going home $HOME"
 cd "$HOME"
 #git config --global user.email "travis@travis-ci.org"
 #git config --global user.name "travis-ci"
-git clone --branch=gh-pages https://github.com/neverwoods/validformbuilder gh-pages || exit 1
+git clone --branch=gh-pages https://github.com/neverwoods/validformbuilder.git gh-pages || exit 1
 
+echo "Entering gh-pages/docs"
 cd gh-pages/docs
 git rm -rf .
 
+echo "Copy generated docs from $TRAVIS_BUILD_DIR/docs to ./"
 cp -rf "$TRAVIS_BUILD_DIR"/docs/* ./ || exit 1
 
 git add .
