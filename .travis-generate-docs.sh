bd863461760cc6d52da1e0d087211c0fe37014e1
Robin van Baalen <robin@neverwoods.com>
2014-06-17 02:14:26 -0400

    Tezzt
    Signed-off-by: Robin van Baalen <robin@neverwoods.com>

--- .travis-generate-docs.sh
+++ .travis-generate-docs.sh
@@ -8,11 +8,17 @@ cd ~
 #git config --global user.email "travis@travis-ci.org"
 #git config --global user.name "travis-ci"
 git clone --branch=gh-pages https://github.com/neverwoods/validformbuilder.git gh-pages || exit 1
-
+echo "-----"
+ls -la .
+echo "-----"
 echo "Entering gh-pages"
 cd gh-pages
-
+echo "-----"
+ls -la .
+echo "-----"
 git rm -rf .
+echo "-----"
+ls -la .
 
 echo "Copy generated docs from $TRAVIS_BUILD_DIR/docs/* to $HOME/gh-pages/docs"
 #cp -Rf $TRAVIS_BUILD_DIR/docs/* $HOME/gh-pages/docs
