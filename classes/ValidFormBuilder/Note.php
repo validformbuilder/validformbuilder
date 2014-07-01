e7dde6f114ef9d4122b368860fff7b13c0abcc42
Robin van Baalen <robin@neverwoods.com>
2014-06-30 20:59:39 -0400

    Finished Note docs

--- classes/ValidFormBuilder/Note.php
+++ classes/ValidFormBuilder/Note.php
@@ -1,6 +1,4 @@
 <?php
-namespace ValidFormBuilder;
-
 /**
  * ValidForm Builder - build valid and secure web forms quickly
  *
@@ -20,19 +18,40 @@ namespace ValidFormBuilder;
  * @link http://validformbuilder.org
  */
 
+namespace ValidFormBuilder;
+
 /**
  * Note Class
  *
+ * @internal
  * @package ValidForm
- * @author Felix Langfeldt
+ * @author Felix Langfeldt <felix@neverwoods.com>
+ * @author Robin van Baalen <robin@neverwoods.com>
+ * @version 3.0.0
  */
 class Note extends Base
 {
-
+    /**
+     * The note header
+     * @internal
+     * @var string
+     */
     protected $__header;
 
+    /**
+     * The note body
+     * @internal
+     * @var string
+     */
     protected $__body;
 
+    /**
+     * Create new Note instance
+     * @internal
+     * @param string $header The note's header
+     * @param string $body The note's body
+     * @param array $meta The meta array
+     */
     public function __construct($header = null, $body = null, $meta = array())
     {
         $this->__header = $header;
@@ -42,6 +61,16 @@ class Note extends Base
         $this->__initializeMeta();
     }
 
+    /**
+     * Render the Note
+     *
+     * @internal
+     * @param boolean $submitted Define if the element has been submitted and propagate that flag to the child fields
+     * @param boolean $blnSimpleLayout Only render in simple layout mode
+     * @param boolean $blnLabel
+     * @param boolean $blnDisplayErrors Display generated errors
+     * @return string Rendered Note
+     */
     public function toHtml()
     {
         $this->setMeta("class", "vf__notes");
