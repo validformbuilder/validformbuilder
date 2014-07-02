0464daae666d8e2dda2ec0fba7bd6f1501912872
Robin van Baalen <robin@neverwoods.com>
2014-07-02 16:25:22 -0400

    Removed unused code, moved to Area subfolder [skip ci]

--- examples/Area/active-area.php
+++ examples/Area/active-area.php
@@ -37,7 +37,6 @@ $objArea->addField(
 
 //*** Generate form output
 if ($objForm->isValid() && $objForm->isSubmitted()) {
-    $strCheckboxValue = $objForm->getValidField("rating")->getValue();
     $strOutput = $objForm->valuesAsHtml();
 } else {
     $strOutput = $objForm->toHtml();
