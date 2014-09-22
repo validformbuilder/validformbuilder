7840948d3206301ea4f955e29de4b07e66c6ed0c
Felix Langfeldt <felix@neverwoods.com>
2014-09-22 10:31:45 -0400

    Disabled validation logic for checkboxes/radios
    This part of the validation was already shady, but is throwing errors now. Disabled it for the time being.

--- js/validform.js
+++ js/validform.js
@@ -1752,7 +1752,8 @@ ValidFormFieldValidator.prototype.validate = function(value) {
                 }
 
                 //*** Check specific types using the type array.
-                // TODO: Validate that the code block below is still needed and working
+                // FIXME: This block throws errors on checkboxes in a list field.
+				/*
                 if (typeof this.check === "object") {
                     for (var intCount = 0; intCount < objValidElements.length; intCount++) {
                         if (!ValidForm.inArray(this.check, objValidElements.get(intCount))) {
@@ -1761,6 +1762,7 @@ ValidFormFieldValidator.prototype.validate = function(value) {
                         }
                     }
                 }
+				*/
 
                 return true;
             } else {
